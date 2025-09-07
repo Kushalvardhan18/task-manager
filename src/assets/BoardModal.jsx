0e58b1ffb9a3db440441a131c6db142d1abc96c3
Kushal Vardhan <vardhank140102@gmail.com>
2025-09-07 17:54:35 +0530

    modal styling done

--- src/assets/BoardModal.jsx
+++ src/assets/BoardModal.jsx
@@ -10,49 +10,58 @@ function BoardModal({ oncreate, onclose }) {
     oncreate(newBoard);
   }
   return (
-    <div className="flex border-1  justify-between gap-4  fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-      <div className="flex flex-col p-2">
-        <div className="flex flex-col gap-2">
-          <span className="flex gap-2 items-center">
-            Name:
-            <input
-              type="text"
-              placeholder="Board Name"
-              value={name}
-              onChange={(e) => setName(e.target.value.toUpperCase())}
-              className="border-1 px-2 py-1 focus:outline-none focus:border-amber-500 focus:border-1 rounded-sm "
-            />
-          </span>
-          <span className="flex gap-2">
-            Color :
-            <input
-              type="color"
-              value={color}
-              onChange={(e) => setColor(e.target.value)}
-            />
-          </span>
-        </div>
-        {name === "" ? (
-          <button className=" !bg-green-300 w-fit px-3 py-1 cursor-not-allowed rounded-md">
-            Create
-          </button>
-        ) : (
+    <div className="fixed inset-0 !bg-black/50">
+      <div className="flex border-1 flex-col justify-between  fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-5 rounded-xl">
+        <div className="flex justify-center items-center">
+          <h1 className="text-2xl font-semibold !text-blue-600">
+            CREATE NEW BOARD
+          </h1>
           <button
-            onClick={() => {
-              board(), onclose();
-            }}
-            className="rounded-md !bg-green-500 w-fit px-3 py-1 hover:cursor-pointer hover:!bg-green-800"
+            className="hover:!text-red-800 px-2 py-1 h-fit m-1 font-bold text-2xl !text-red-500"
+            onClick={onclose}
           >
-            Create
+            X
           </button>
-        )}
+        </div>
+        <div className="flex flex-col p-2">
+          <div className="flex flex-col gap-5">
+            <div className="flex gap-2 items-center">
+              <label>Name:</label>
+              <input
+                type="text"
+                placeholder="Board Name"
+                value={name}
+                onChange={(e) => setName(e.target.value.toUpperCase())}
+                className="border-1 px-2 py-1 focus:outline-none focus:border-amber-500 focus:border-1 rounded-sm "
+              />
+            </div>
+            <div className="flex gap-2">
+              <label>Color :</label>
+              <input
+                type="color"
+                value={color}
+                onChange={(e) => setColor(e.target.value)}
+              />
+            </div>
+          </div>
+          <div className="flex justify-center mt-5">
+            {name === "" ? (
+              <button className=" !bg-green-300 w-fit px-5 py-2 cursor-not-allowed rounded-md">
+                Create
+              </button>
+            ) : (
+              <button
+                onClick={() => {
+                  board(), onclose();
+                }}
+                className="rounded-md !bg-green-500 w-fit px-5 py-2 hover:cursor-pointer hover:!bg-green-800"
+              >
+                Create
+              </button>
+            )}
+          </div>
+        </div>
       </div>
-      <button
-        className="hover:!text-red-500 px-2 py-1 h-fit m-1 font-bold text-xl"
-        onClick={onclose}
-      >
-        X
-      </button>
     </div>
   );
 }
